9eee49c9d5fe9916e7fce493d91a92179bedfa89
Ajit Bohra <ajit@lubus.in>
2018-02-24 06:15:32 +0530

    Add uninstall cleanup

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,13 @@
 <?php
-
+ // Exit if accessed directly.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
+
+// Clear option(s)
+delete_option( 'taf_settings' );
+
+// clear transient(s)
+delete_transient( 'taf_tweet_cache' );
 
 ?>
